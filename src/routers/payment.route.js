a2c2ba70ecb4a6d5079f987beba83a6989c523e9
ducnm <nguyenduc100xh@gmail.com>
2025-11-01 22:15:55 +0700

    fix zalopay

--- src/routers/payment.route.js
+++ src/routers/payment.route.js
@@ -2,17 +2,31 @@
 import express from "express";
 import paymentController from "../controllers/payment.controller.js";
 import momoController from "../controllers/momo.controller.js";
-import { optionalAuth /*, authenticateToken */ } from "../middleware/auth.middleware.js";
-console.log('[ROUTE] payment.route.js loaded');
+import zaloController from "../controllers/paymentZalo.controller.js";
+import {
+  optionalAuth /*, authenticateToken */,
+} from "../middleware/auth.middleware.js";
+console.log("[ROUTE] payment.route.js loaded");
 
 const router = express.Router();
 
 // ---------- PUBLIC callbacks (no auth) ----------
 router.get("/vnpay/return", paymentController.vnpayReturn);
-router.post("/vnpay/ipn", express.urlencoded({ extended: false }), paymentController.vnpayIPN);
+router.post(
+  "/vnpay/ipn",
+  express.urlencoded({ extended: false }),
+  paymentController.vnpayIPN
+);
 
 router.get("/momo/return", momoController.momoReturn);
 router.post("/momo/ipn", express.json(), momoController.momoIPN);
+
+router.post(
+  "/zalopay/callback",
+  express.urlencoded({ extended: false }),
+  zaloController.zaloCallback
+);
+router.get("/zalopay/return", zaloController.zaloReturn);
 
 // ---------- From this point, we DO NOT apply a global authenticateToken middleware.
 // Instead, apply optionalAuth or authenticateToken per-route as needed.
@@ -21,6 +35,7 @@ router.post("/momo/ipn", express.json(), momoController.momoIPN);
 // but if client sends Authorization: Bearer <token> then req.user will be available.
 router.post("/vnpay/create", optionalAuth, paymentController.createPayment);
 router.post("/momo/create", optionalAuth, momoController.createPayment);
+router.post("/zalopay/create", zaloController.createZaloOrder);
 
 // If you want create to require login, replace optionalAuth with authenticateToken:
 // router.post("/vnpay/create", authenticateToken, paymentController.createPayment);
